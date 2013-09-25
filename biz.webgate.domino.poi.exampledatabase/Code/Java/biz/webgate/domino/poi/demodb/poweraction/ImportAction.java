374f2b5e5de43baaacfc4305ab4e40c8f9520dae
Christian Guedemann <christian.guedemann@webgate.biz>
2013-09-25 09:57:40 +0200

    FileUpload TestCase

---
+++ biz.webgate.domino.poi.exampledatabase/Code/Java/biz/webgate/domino/poi/demodb/poweraction/ImportAction.java
@@ -0,0 +1,41 @@
+/*
+ * © Copyright WebGate Consulting AG, 2013
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at:
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ * implied. See the License for the specific language governing 
+ * permissions and limitations under the License.
+ */
+package biz.webgate.domino.poi.demodb.poweraction;
+
+import java.io.File;
+import java.util.HashMap;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import biz.webgate.dominoext.poi.util.AbstractPOIPowerAction;
+
+public class ImportAction extends AbstractPOIPowerAction<Workbook> {
+
+	@Override
+	protected Workbook doItPriv(Workbook arg0, HashMap<String, String> hsCurrent) {
+		try {
+			System.out.println("Accessing -> "+hsCurrent.get("FILE"));
+			File excelFile = new java.io.File(hsCurrent.get("FILE"));
+			Workbook wb2 = WorkbookFactory.create(excelFile);
+			System.out.println("###Got the file and first sheetName is -> " + wb2.getSheetName(0));
+			return wb2;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
