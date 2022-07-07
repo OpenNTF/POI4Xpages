5d31dd4cfd06ce24f881ccd0d67c34b53e8de0ad
PhilippeArdit <philippe.ardit@free.fr>
2022-07-07 10:22:57 +0200

    added UIDocument.processRequestAndGetStream(), generateNewByteArrayOutputStream(), processBookmarks2TextBox(), generateNewByteArrayOutputStream(), updated processBookmarks2Document()

--- poi4xpages/bundles/biz.webgate.dominoext.poi/src/biz/webgate/dominoext/poi/component/containers/UIDocument.java
+++ poi4xpages/bundles/biz.webgate.dominoext.poi/src/biz/webgate/dominoext/poi/component/containers/UIDocument.java
@@ -1,26 +1,29 @@
 /**
  * Copyright (c) 2012-2021 WebGate Consulting AG and others
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package biz.webgate.dominoext.poi.component.containers;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.security.AccessController;
 import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.faces.component.UIComponentBase;
 import javax.faces.context.FacesContext;
@@ -29,16 +32,18 @@ import javax.faces.el.ValueBinding;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
+import biz.webgate.dominoext.poi.component.data.ITemplateSource;
+import biz.webgate.dominoext.poi.component.data.document.IDocumentBookmark;
+import biz.webgate.dominoext.poi.component.data.document.table.DocumentTable;
+import biz.webgate.dominoext.poi.component.data.image.IBookmarkImage;
+import biz.webgate.dominoext.poi.component.kernel.DocumentProcessor;
+import biz.webgate.dominoext.poi.utils.logging.LoggerFactory;
+
 import com.ibm.commons.util.StringUtil;
 import com.ibm.xsp.binding.MethodBindingEx;
 import com.ibm.xsp.component.FacesAjaxComponent;
 import com.ibm.xsp.util.FacesUtil;
 import com.ibm.xsp.util.StateHolderUtil;
-
-import biz.webgate.dominoext.poi.component.data.ITemplateSource;
-import biz.webgate.dominoext.poi.component.data.document.IDocumentBookmark;
-import biz.webgate.dominoext.poi.component.data.document.table.DocumentTable;
-import biz.webgate.dominoext.poi.component.kernel.DocumentProcessor;
 
 public class UIDocument extends UIComponentBase implements FacesAjaxComponent {
 
@@ -48,6 +53,7 @@ public class UIDocument extends UIComponentBase implements FacesAjaxComponent {
 
 	private List<IDocumentBookmark> m_Bookmarks;
 	private List<DocumentTable> m_Tables;
+	private List<IBookmarkImage> m_BookmarksImages;
 	private String m_pathInfo;
 	private String m_DownloadFileName;
 	private ITemplateSource m_TemplateSource;
@@ -70,11 +76,26 @@ public class UIDocument extends UIComponentBase implements FacesAjaxComponent {
 
 	public void addBookmark(IDocumentBookmark myBookmark) {
 		if (m_Bookmarks == null) {
-			m_Bookmarks = new ArrayList<>();
+			m_Bookmarks = new ArrayList<IDocumentBookmark>();
 		}
 		m_Bookmarks.add(myBookmark);
 	}
 
+	public List<IBookmarkImage> getBookmarksImages() {
+		return m_BookmarksImages;
+	}
+
+	public void setBookmarksImages(List<IBookmarkImage> bookmarksImages) {
+		m_BookmarksImages = bookmarksImages;
+	}
+
+	public void addBookmarkImage(IBookmarkImage myBookmarkImage) {
+		if (m_Bookmarks == null) {
+			m_Bookmarks = new ArrayList<IDocumentBookmark>();
+		}
+		m_BookmarksImages.add(myBookmarkImage);
+	}
+	
 	public List<DocumentTable> getTables() {
 		return m_Tables;
 	}
@@ -85,7 +106,7 @@ public class UIDocument extends UIComponentBase implements FacesAjaxComponent {
 
 	public void addTables(DocumentTable myTable) {
 		if (m_Tables == null) {
-			m_Tables = new ArrayList<>();
+			m_Tables = new ArrayList<DocumentTable>();
 		}
 		m_Tables.add(myTable);
 	}
@@ -151,7 +172,6 @@ public class UIDocument extends UIComponentBase implements FacesAjaxComponent {
 		m_PdfOutput = pdfOutput;
 	}
 
-	@Override
 	public boolean handles(FacesContext context) {
 		String reqPathInfo = context.getExternalContext().getRequestPathInfo();
 		if (StringUtil.isNotEmpty(reqPathInfo)) {
@@ -178,16 +198,33 @@ public class UIDocument extends UIComponentBase implements FacesAjaxComponent {
 		return false;
 	}
 
-	@Override
 	public void processAjaxRequest(FacesContext context) throws IOException {
 		DocumentProcessor.INSTANCE.processCall(context, this, false, null);
+	}
+
+	public ByteArrayOutputStream processRequestAndGetStream() throws IOException {
+		ByteArrayOutputStream bos = null;
+		Logger logCurrent = LoggerFactory.getLogger(this.getClass().getCanonicalName());
+
+		ITemplateSource itsCurrent = getTemplateSource();
+		if (itsCurrent == null) {
+			logCurrent.severe("No Template found!");
+			return bos;
+		}
+		logCurrent.info("Start processing UIDocument generation");
+		try {
+			bos = DocumentProcessor.INSTANCE.generateNewByteArrayOutputStream(itsCurrent, getBookmarks(), getTables(), getBookmarksImages(), getFacesContext(), this);
+		} catch (Exception e) {
+			logCurrent.log(Level.SEVERE, "Error in UIGeneration", e);
+		}
+		return bos;
 	}
 
 	// SAVING AND RESTORING
 	@Override
 	public Object saveState(FacesContext context) {
 		try {
-			Object[] state = new Object[8];
+			Object[] state = new Object[9];
 			state[0] = super.saveState(context);
 			state[1] = m_DownloadFileName;
 			state[2] = m_pathInfo;
@@ -196,6 +233,7 @@ public class UIDocument extends UIComponentBase implements FacesAjaxComponent {
 			state[5] = StateHolderUtil.saveMethodBinding(context, m_PostGenerationProcess);
 			state[6] = m_PdfOutput;
 			state[7] = StateHolderUtil.saveList(context, m_Tables);
+			state[8] = StateHolderUtil.saveList(context, m_BookmarksImages);
 			return state;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -214,6 +252,7 @@ public class UIDocument extends UIComponentBase implements FacesAjaxComponent {
 		m_PostGenerationProcess = StateHolderUtil.restoreMethodBinding(context, this, state[5]);
 		m_PdfOutput = (Boolean) state[6];
 		m_Tables = StateHolderUtil.restoreList(context, this, state[7]);
+		m_BookmarksImages = StateHolderUtil.restoreList(context, this, state[8]);
 
 	}
 
@@ -236,23 +275,26 @@ public class UIDocument extends UIComponentBase implements FacesAjaxComponent {
 			/*
 			 * if (FacesUtil.isCancelled(m_PostGenerationProcess.invoke(context,
 			 * params))) { return false; }
-			 */
+			*/
 			doPostGenerationProcessPrivileged(context, params);
 			return true;
 		}
 		return true;
 	}
-
+	
 	public void doPostGenerationProcessPrivileged(final FacesContext context, final Object[] params) {
 		try {
-			AccessController.doPrivileged((PrivilegedExceptionAction<Void>) () -> {
-				m_PostGenerationProcess.invoke(context, params);
-				return null;
+			AccessController.doPrivileged(new PrivilegedExceptionAction<Void>() {
+				public Void run() throws Exception {
+					m_PostGenerationProcess.invoke(context, params);
+					return null;
+				}
 			});
 		} catch (PrivilegedActionException e) {
 			e.printStackTrace();
 		}
 	}
 
+	
 	private static final String[] s_postGenParamNames = { "xwpfdocument" }; // $NON-NLS-1$
 }
---
+++ poi4xpages/bundles/biz.webgate.dominoext.poi/src/biz/webgate/dominoext/poi/component/data/image/IBookmarkImage.java
@@ -0,0 +1,11 @@
+package biz.webgate.dominoext.poi.component.data.image;
+
+public interface IBookmarkImage {
+	public String getName();
+	public int getId();
+	public String getFileName();
+	public String getFileType();
+	public int getExtension();
+	public int getWidth();
+	public int getHeight();
+}
